4ee7e4c4bf8e26e94b75b2ca44cfa75477366ef7
Kanan Farzali <kanan.farzali@gmail.com>
2024-12-07 23:58:20 +0100

    adding dummy home and about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HomePage = () => {
-  return <div className="text-red-500">Home</div>;
+  return <div className="text-red-500">Home Page</div>;
 };
 
 export default HomePage;
